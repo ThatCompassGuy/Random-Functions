58d074918a05f12567c99f88edeeab2e343b4ca5
TmGL <alanharuwilliams@icloud.com>
2021-03-13 01:05:30 +0000

    renamed files

--- util/index.ts
+++ util/index.ts
@@ -1,6 +1,6 @@
-import fromCelsius from './units/fromCelsius';
-import fromKelvin from './units/fromKelvin';
-import fromFahrenheit from './units/fromFahrenheit';
+import fromCelsius from './units/celsius';
+import fromKelvin from './units/kelvin';
+import fromFahrenheit from './units/fahrenheit';
 import Options from './interfaces/options';
 
 export { fromCelsius, fromFahrenheit, fromKelvin, Options };
