4ededcf16128c146dc6ac3472a0e8cde3397d41f
TmGL <alanharuwilliams@icloud.com>
2021-03-17 23:03:28 +0000

    test(*): update and fix tests

--- test/functions.test.ts
+++ test/functions.test.ts
@@ -39,21 +39,26 @@ test('convert temperatures', () => {
     expect(convert(1, { from: 'celsius', to: 'fahrenheit' })).toBe('33.8°F');
     expect(convert(1, { from: 'celsius', to: 'kelvin' })).toBe('274.15K');
     expect(convert(1, { from: 'celsius', to: 'celsius' })).toBe('1°C');
-    expect(convert(2, { from: 'celsius', to: 'fahrenheit' })).toBe('35.6°F');
-    expect(convert(2, { from: 'celsius', to: 'kelvin' })).toBe('275.15K');
-    expect(convert(2, { from: 'celsius', to: 'celsius' })).toBe('2°C');
+    // with rounding
+    expect(convert(2, { from: 'celsius', to: 'fahrenheit', round: 1 })).toBe('35.6°F');
+    expect(convert(2, { from: 'celsius', to: 'kelvin', round: 1 })).toBe('275.1K');
+    expect(convert(2, { from: 'celsius', to: 'celsius', round: 1 })).toBe('2.0°C');
+
     // from kelvin
     expect(convert(1, { from: 'kelvin', to: 'fahrenheit' })).toBe('-457.87°F');
     expect(convert(1, { from: 'kelvin', to: 'celsius' })).toBe('-272.15°C');
     expect(convert(1, { from: 'kelvin', to: 'kelvin' })).toBe('1K');
-    expect(convert(2, { from: 'kelvin', to: 'fahrenheit' })).toBe('-456.07°F');
-    expect(convert(2, { from: 'kelvin', to: 'celsius' })).toBe('-271.15°C');
-    expect(convert(2, { from: 'kelvin', to: 'kelvin' })).toBe('2K');
+    // with rounding
+    expect(convert(2, { from: 'kelvin', to: 'fahrenheit', round: 1 })).toBe('-456.1°F');
+    expect(convert(2, { from: 'kelvin', to: 'celsius', round: 1 })).toBe('-271.1°C');
+    expect(convert(2, { from: 'kelvin', to: 'kelvin', round: 1 })).toBe('2.0K');
+
     // from fahrenheit
-    expect(convert(1, { from: 'fahrenheit', to: 'kelvin' })).toBe('255.928K');
-    expect(convert(1, { from: 'fahrenheit', to: 'celsius' })).toBe('-17.2222°C');
+    expect(convert(1, { from: 'fahrenheit', to: 'kelvin' })).toBe('255.92777777777775K');
+    expect(convert(1, { from: 'fahrenheit', to: 'celsius' })).toBe('-17.22222222222222°C');
     expect(convert(1, { from: 'fahrenheit', to: 'fahrenheit' })).toBe('1°F');
-    expect(convert(2, { from: 'fahrenheit', to: 'kelvin' })).toBe('256.483K');
-    expect(convert(2, { from: 'fahrenheit', to: 'celsius' })).toBe('-16.6667°C');
-    expect(convert(2, { from: 'fahrenheit', to: 'fahrenheit' })).toBe('2°F');
+    // with rounding
+    expect(convert(2, { from: 'fahrenheit', to: 'kelvin', round: 1 })).toBe('256.5K');
+    expect(convert(2, { from: 'fahrenheit', to: 'celsius', round: 1 })).toBe('-16.7°C');
+    expect(convert(2, { from: 'fahrenheit', to: 'fahrenheit', round: 1 })).toBe('2.0°F');
 });
