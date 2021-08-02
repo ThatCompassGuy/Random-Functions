2eb414048523725b98bcd85ea11bf693a3dd4dad
TmGL <alanharuwilliams@icloud.com>
2021-08-02 12:53:11 +0100

    test: use 'toStrictEqual' instead of 'toBe'

--- test/functions.test.ts
+++ test/functions.test.ts
@@ -35,27 +35,27 @@ test('reverse strings', () => {
 
 test('convert temperatures', () => {
     // from celsius
-    expect(convert(1, { from: 'celsius', to: 'fahrenheit' })).toBe('33.8°F');
-    expect(convert(1, { from: 'celsius', to: 'kelvin' })).toBe('274.15K');
-    expect(convert(1, { from: 'celsius', to: 'celsius' })).toBe('1°C');
+    expect(convert(1, { from: 'celsius', to: 'fahrenheit' })).toStrictEqual('33.8°F');
+    expect(convert(1, { from: 'celsius', to: 'kelvin' })).toStrictEqual('274.15K');
+    expect(convert(1, { from: 'celsius', to: 'celsius' })).toStrictEqual('1°C');
     // with rounding
-    expect(convert(2, { from: 'celsius', to: 'fahrenheit', round: 1 })).toBe('35.6°F');
-    expect(convert(2, { from: 'celsius', to: 'kelvin', round: 1 })).toBe('275.1K');
-    expect(convert(2, { from: 'celsius', to: 'celsius', round: 1 })).toBe('2.0°C');
+    expect(convert(2, { from: 'celsius', to: 'fahrenheit', round: 1 })).toStrictEqual('35.6°F');
+    expect(convert(2, { from: 'celsius', to: 'kelvin', round: 1 })).toStrictEqual('275.1K');
+    expect(convert(2, { from: 'celsius', to: 'celsius', round: 1 })).toStrictEqual('2.0°C');
 
     // from kelvin
-    expect(convert(1, { from: 'kelvin', to: 'fahrenheit' })).toBe('-457.87°F');
-    expect(convert(1, { from: 'kelvin', to: 'celsius' })).toBe('-272.15°C');
-    expect(convert(1, { from: 'kelvin', to: 'kelvin' })).toBe('1K');
+    expect(convert(1, { from: 'kelvin', to: 'fahrenheit' })).toStrictEqual('-457.87°F');
+    expect(convert(1, { from: 'kelvin', to: 'celsius' })).toStrictEqual('-272.15°C');
+    expect(convert(1, { from: 'kelvin', to: 'kelvin' })).toStrictEqual('1K');
     // with rounding
-    expect(convert(2, { from: 'kelvin', to: 'fahrenheit', round: 1 })).toBe('-456.1°F');
-    expect(convert(2, { from: 'kelvin', to: 'celsius', round: 1 })).toBe('-271.1°C');
-    expect(convert(2, { from: 'kelvin', to: 'kelvin', round: 1 })).toBe('2.0K');
+    expect(convert(2, { from: 'kelvin', to: 'fahrenheit', round: 1 })).toStrictEqual('-456.1°F');
+    expect(convert(2, { from: 'kelvin', to: 'celsius', round: 1 })).toStrictEqual('-271.1°C');
+    expect(convert(2, { from: 'kelvin', to: 'kelvin', round: 1 })).toStrictEqual('2.0K');
 
     // from fahrenheit
-    expect(convert(1, { from: 'fahrenheit', to: 'kelvin' })).toBe('255.92777777777775K');
-    expect(convert(1, { from: 'fahrenheit', to: 'celsius' })).toBe('-17.22222222222222°C');
-    expect(convert(1, { from: 'fahrenheit', to: 'fahrenheit' })).toBe('1°F');
+    expect(convert(1, { from: 'fahrenheit', to: 'kelvin' })).toStrictEqual('255.92777777777775K');
+    expect(convert(1, { from: 'fahrenheit', to: 'celsius' })).toStrictEqual('-17.22222222222222°C');
+    expect(convert(1, { from: 'fahrenheit', to: 'fahrenheit' })).toStrictEqual('1°F');
     // with rounding
     expect(convert(2, { from: 'fahrenheit', to: 'kelvin', round: 1 })).toStrictEqual('256.5K');
     expect(convert(2, { from: 'fahrenheit', to: 'celsius', round: 1 })).toStrictEqual('-16.7°C');
