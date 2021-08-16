a1167203e80c00bdb3c45c785352ab7944fec0fc
TmGL <alanharuwilliams@icloud.com>
2021-08-16 04:45:01 +0100

    refractor(functions): use maps

--- src/functions/findDupe.ts
+++ src/functions/findDupe.ts
@@ -5,15 +5,14 @@ export default function findDupe(nums: any[]) : number | number[] | undefined  {
     
     nums = nums.filter((num) => !isNaN(num));
 
-    const duplicates = [];
-    let res = false;
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i + 1] === arr[i]) {
-            duplicates.push(parseInt(arr[i]));
-            res = true;
+    const seen = new Map();
+    const results: number[] = [];
+    for (let num of nums) {
+        num = parseInt(num);
+        if (seen.has(num) && results.indexOf(num) == -1) {
+            results.push(num);
         }
+        seen.set(num, null);
     }
-
-    return res ? duplicates : undefined;
-}    return results.length == 1 ? results[0] : results.length == 0 ? undefined : results;
+    return results.length == 1 ? results[0] : results.length == 0 ? undefined : results;
+}
